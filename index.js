79a48e7e36e8eef959462a29b2a99d2cbf1e88b3
anangs <nanang_s1@yahoo.com>
2023-08-18 09:03:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,93 +3,22 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const { EditPhotoHandler } = require('./feature/edit_foto');
 const { ChatAIHandler } = require('./feature/chat_ai');
 const { ImageAIHandler } = require('./feature/image_ai');
-const QRCode = require('qrcode')
 
 
-//const app = express();
-
-/* const client = new Client({
-    puppeteer: { headless: true },
-  }); */
 
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 
-//app.use(express.static('public'));
-
-//app.get('/', (req, res) => {
- //   res.sendFile(__dirname + '/index.html');
- //});
-  
-//   app.get('/qrcode', async (req, res) => {
-//     try {
-//       const qrCodeData = await client.getQRCode();
-//       const qrCodeBase64 = await qrcode.toDataURL(qrCodeData, { scale: 8 });
-//       res.send(`<img src="${qrCodeBase64}" alt="QR Code">`);
-//     } catch (err) {
-//       console.error(err);
-//       res.send('Error generating QR code');
-//     }
-//   });
-  
-// Define a route to display the QR code image
-
-// app.get('/qrcode', function(req, res) {  
-// client.on('qr', (qr) => {
-
-//     var code = qr.image(qr, { type: 'svg' });
-//     res.type('svg');
-//     code.pipe(res);
-//    // res.send(qrCodeData);
-
-// });
-//   });
 
 
-
-// app.get('/', async (req, res) => {
-
-
-
-//     res.send('<h2>QRCode Generated</h2><div><img src="http://localhost:3000/qrcodes.png"/></div>')
-
-
-// });
-
-
-client.on('qr', (qr) => {
-    QRCode.toFile('qrcode.png', qr, {
-        errorCorrectionLevel: 'H',
-        type: 'png',
-        quality: 1,
-        margin: 1
-      }, (err) => {
-        if (err) throw err;
-        console.log('QR code saved as qrcode.png');
-      }); 
- 
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
 });
 
-// Start the server
-// app.listen(80, () => {
-//   console.log('Server listening on port 3000');
-// });
-
- 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
-
-//   client.on('qr', async (qr) => {
-//     try {
-//       const qrCodeData = await client.generateQR(qr);
-//       const qrCodeBase64 = qrCodeData.replace(/^data:image\/(png|jpg);base64,/, '');
-//       io.emit('qrcode', qrCodeBase64);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   });
 
 client.on('message', async msg => {
 
@@ -116,11 +45,3 @@ client.on('message', async msg => {
 });
 
 client.initialize();
-
-
- 
-// const qrcodeUrl = 'http://localhost:3000/qrcode';
-// require('open')(qrcodeUrl);
-
-
-
